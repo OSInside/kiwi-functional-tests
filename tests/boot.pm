fb09ae7b73761962bc36bcac03c4b54450806252
Marcus Schäfer <ms@suse.de>
2020-10-12 12:23:04 +0200

    Added simple boot test

---
+++ tests/boot.pm
@@ -0,0 +1,43 @@
+# Copyright (C) 2014 SUSE Linux GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use base "basetest";
+use strict;
+use testapi;
+
+sub run {
+    wait_serial "localhost login:", 900;
+    sleep(2);
+    type_string "root";
+    send_key "ret";
+    sleep(1);
+    type_string "linux";
+    send_key "ret";
+    sleep(2);
+    assert_script_run(
+        "systemd-analyze blame | head -n 15 | tee -a /dev/$serialdev"
+    );
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
