040fdc9677647fabfc5a4f7f4c932a3fba0b5b00
Dan Čermák <dcermak@suse.com>
2021-11-05 07:29:35 +0100

    Make perlcritic a bit happier

--- tests/boot.pm
+++ tests/boot.pm
@@ -14,7 +14,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-use base "basetest";
+use base 'basetest';
+use warnings;
 use strict;
 use testapi;
 
--- tests/login.pm
+++ tests/login.pm
@@ -14,8 +14,9 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-use base "basetest";
+use base 'basetest';
 use strict;
+use warnings;
 use testapi;
 
 sub run {
